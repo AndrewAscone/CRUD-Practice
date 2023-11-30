a958faefa778c3912f9e4bba0f98943594e41e18
AndrewAscone <andrew.ascone@gmail.com>
2023-11-30 18:53:15 -0500

    Updated pom.xml, adding fields to band entity

--- src/main/java/com/Scone/CRUD/Practice/Band.java
+++ src/main/java/com/Scone/CRUD/Practice/Band.java
@@ -1,4 +1,16 @@
 package com.Scone.CRUD.Practice;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Band {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String bandName;
+    private String genre;
+    private int yearFormed;
 }
