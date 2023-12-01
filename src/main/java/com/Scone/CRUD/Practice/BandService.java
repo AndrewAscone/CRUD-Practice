1ebd624de1b2f226d3bd2cbafb2bbc8ca1ce188c
AndrewAscone <andrew.ascone@gmail.com>
2023-12-01 18:57:33 -0500

    Started filling out service file

--- src/main/java/com/Scone/CRUD/Practice/BandService.java
+++ src/main/java/com/Scone/CRUD/Practice/BandService.java
@@ -1,4 +1,16 @@
 package com.Scone.CRUD.Practice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class BandService {
+    private BandRepository repository;
+
+    @Autowired
+    public BandService(BandRepository repository){
+        this.repository = repository;
+    }
+
+    public Band create(Band band){
+        return repository.save(band);
+    }
 }
