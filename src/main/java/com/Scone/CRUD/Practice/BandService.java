b25bb0714480549acab5c2a6ab4e3334522f07c3
AndrewAscone <andrew.ascone@gmail.com>
2023-12-01 19:15:21 -0500

    Added remaining functions to service

--- src/main/java/com/Scone/CRUD/Practice/BandService.java
+++ src/main/java/com/Scone/CRUD/Practice/BandService.java
@@ -2,6 +2,9 @@ package com.Scone.CRUD.Practice;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BandService {
     private BandRepository repository;
 
@@ -12,5 +15,31 @@ public class BandService {
 
     public Band create(Band band){
         return repository.save(band);
+    }
+
+    public Band readById(Long id){
+        return repository.findById(id).get();
+    }
+
+    public List<Band> readAll(){
+        Iterable<Band> allBands = repository.findAll();
+        List<Band> bandList = new ArrayList<>();
+        allBands.forEach(bandList::add);
+        return bandList;
+    }
+
+    public Band update(Long id, Band newBandData){
+        Band bandInDatabase = this.readById(id);
+        bandInDatabase.setBandName(newBandData.getBandName());
+        bandInDatabase.setGenre(newBandData.getGenre());
+        bandInDatabase.setYearFormed(newBandData.getYearFormed());
+        bandInDatabase = repository.save(bandInDatabase);
+        return bandInDatabase;
+    }
+
+    public Band deleteById(Long id){
+        Band bandToBeDeleted = this.readById(id);
+        repository.delete(bandToBeDeleted);
+        return bandToBeDeleted;
     }
 }
