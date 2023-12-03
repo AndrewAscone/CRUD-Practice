641c235b53b3c40de1a04ab1bc64937e34bca378
AndrewAscone <andrew.ascone@gmail.com>
2023-12-02 20:19:30 -0500

    Began controller file

--- src/main/java/com/Scone/CRUD/Practice/BandController.java
+++ src/main/java/com/Scone/CRUD/Practice/BandController.java
@@ -1,4 +1,14 @@
 package com.Scone.CRUD.Practice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class BandController {
+    private BandService service;
+
+    @Autowired
+    public BandController(BandService service){
+        this.service = service;
+    }
 }
