42049c80bec0ea0ba53abdac0be7194ea938aa2a
AndrewAscone <andrew.ascone@gmail.com>
2023-12-03 19:31:02 -0500

    Made create function in controller

--- src/main/java/com/Scone/CRUD/Practice/BandController.java
+++ src/main/java/com/Scone/CRUD/Practice/BandController.java
@@ -1,7 +1,11 @@
 package com.Scone.CRUD.Practice;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class BandController {
@@ -13,4 +17,8 @@ public class BandController {
     }
     //create, read by id, read all, update by id, delete by id
     //endpoints: PostMapping, GetMapping, PutMapping, DeleteMapping
+    @PostMapping(value = "/create")
+    public ResponseEntity<Band> create(@RequestBody Band band){
+        return new ResponseEntity<>(service.create(band), HttpStatus.CREATED);
+    }
 }
