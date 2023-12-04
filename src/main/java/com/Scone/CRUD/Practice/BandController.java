11fbfda5760464434c3e560c2f8f7d31de5b709f
AndrewAscone <andrew.ascone@gmail.com>
2023-12-04 15:42:33 -0500

    Added read by id and read all methods to controller

--- src/main/java/com/Scone/CRUD/Practice/BandController.java
+++ src/main/java/com/Scone/CRUD/Practice/BandController.java
@@ -4,8 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @Controller
 public class BandController {
@@ -15,10 +19,20 @@ public class BandController {
     public BandController(BandService service){
         this.service = service;
     }
-    //create, read by id, read all, update by id, delete by id
+    //create*, read by id, read all, update by id, delete by id
     //endpoints: PostMapping, GetMapping, PutMapping, DeleteMapping
     @PostMapping(value = "/create")
     public ResponseEntity<Band> create(@RequestBody Band band){
         return new ResponseEntity<>(service.create(band), HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/read/{id}")
+    public ResponseEntity<Band> readById(@PathVariable Long id){
+        return new ResponseEntity<>(service.readById(id), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/readAll")
+    public ResponseEntity<List<Band>> readAll(){
+        return new ResponseEntity<>(service.readAll(), HttpStatus.OK);
     }
 }
