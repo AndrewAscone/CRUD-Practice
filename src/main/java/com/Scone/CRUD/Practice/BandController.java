5828865e63ac3ac3cbecc0dadcb893c9a751eff8
AndrewAscone <andrew.ascone@gmail.com>
2023-12-05 18:02:21 -0500

    Added update by id function

--- src/main/java/com/Scone/CRUD/Practice/BandController.java
+++ src/main/java/com/Scone/CRUD/Practice/BandController.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -34,5 +31,10 @@ public class BandController {
     @GetMapping(value = "/readAll")
     public ResponseEntity<List<Band>> readAll(){
         return new ResponseEntity<>(service.readAll(), HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/update/{id}")
+    public ResponseEntity<Band> updateById(@PathVariable Long id, @RequestBody Band newData){
+        return new ResponseEntity<>(service.update(id, newData), HttpStatus.OK);
     }
 }
