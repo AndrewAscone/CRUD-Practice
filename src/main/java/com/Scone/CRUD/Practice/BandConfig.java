4cffe2b67c0a2c6cb735dd99ad1905114d3949a9
AndrewAscone <andrew.ascone@gmail.com>
2023-12-08 19:04:32 -0500

    Adding annotations

--- src/main/java/com/Scone/CRUD/Practice/BandConfig.java
+++ src/main/java/com/Scone/CRUD/Practice/BandConfig.java
@@ -1,4 +1,10 @@
 package com.Scone.CRUD.Practice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class BandConfig {
+    @Autowired
+    private BandRepository repository;
 }
