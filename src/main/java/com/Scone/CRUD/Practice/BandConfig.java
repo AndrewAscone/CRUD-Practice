d8e39581ee7dc2a1c3a4e4772438fc76e70492ea
AndrewAscone <andrew.ascone@gmail.com>
2023-12-09 19:41:00 -0500

    More config work

--- src/main/java/com/Scone/CRUD/Practice/BandConfig.java
+++ src/main/java/com/Scone/CRUD/Practice/BandConfig.java
@@ -3,8 +3,15 @@ package com.Scone.CRUD.Practice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PostConstruct;
+
 @Configuration
 public class BandConfig {
     @Autowired
     private BandRepository repository;
+
+    @PostConstruct
+    public void setup(){
+
+    }
 }
