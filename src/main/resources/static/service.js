00c184827026d9ad2dbe09c676c7d6e9b5092aa3
AndrewAscone <andrew.ascone@gmail.com>
2023-12-26 18:37:38 -0500

    Starting work on service.js file

---
+++ src/main/resources/static/service.js
@@ -0,0 +1,8 @@
+class Band {
+    constructor(id, bandName, bandGenre, bandYear){
+        this.id = id;
+        this.bandName = bandName;
+        this.bandGenre = bandGenre;
+        this.bandYear = bandYear;
+    }
+}
