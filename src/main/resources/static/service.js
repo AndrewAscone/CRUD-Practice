4be2714c591ba58515310887feb505fa8698e3a8
AndrewAscone <andrew.ascone@gmail.com>
2024-01-09 18:38:41 -0500

    More work on the create function

--- src/main/resources/static/service.js
+++ src/main/resources/static/service.js
@@ -16,4 +16,13 @@ function create(event){
     const bandNameElement = document.getElementById("band-name");
     const bandGenreElement = document.getElementById("band-genre");
     const bandYearElement = document.getElementById("band-year-formed");
+
+    const bandIdValue = bandIdElement.value;
+    const bandNameValue = bandNameElement.value;
+    const bandGenreValue = bandGenreElement.value;
+    const bandYearValue = bandYearElement.value;
+    const band = new Band(bandIdValue, bandNameValue, bandGenreValue, bandYearValue);
+
+    const bandData = JSON.stringify(band);
+    console.log(bandData);
 }
