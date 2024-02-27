c4a4a0e79bd2282ef7e57b1c39368a39ba78a051
AndrewAscone <andrew.ascone@gmail.com>
2024-02-27 16:28:27 -0500

    Added fields, getters and setters to band member

--- src/main/java/com/Scone/CRUD/Practice/BandMember.java
+++ src/main/java/com/Scone/CRUD/Practice/BandMember.java
@@ -1,12 +1,41 @@
 package com.Scone.CRUD.Practice;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class BandMember {
     @Id
     @GeneratedValue
     private long id;
+
+    private String name;
+    private String role;
+
+    @ManyToOne
+    @JoinColumn(name = "band_id")
+    private Band band;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
